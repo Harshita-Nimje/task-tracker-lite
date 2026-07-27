d15857cf8b8ba5871514818ef048d75741baa794
Harshita Nimje <140044690+Harshita-Nimje@users.noreply.github.com>
2026-07-28 03:00:25 +0530

    commit

--- server.js
+++ server.js
@@ -1,61 +1,3 @@
-// // const express = require("express");
-// // const app = express();
-
-// // app.use(express.json());
-
-// // // ✅ Import routes
-// // const protectedRoutes = require("./routes/protectedRoutes");
-
-// // // ✅ Use routes
-// // app.use("/api", protectedRoutes);
-
-// // console.log("Routes loaded");
-
-// // // ✅ Start server
-// // const PORT = 5000;
-// // app.listen(PORT, () => {
-// //     console.log(`Server running on port ${PORT}`);
-// // });
-
-// // import express from "express";
-
-// // const app = express();
-
-// // console.log("🔥 SERVER STARTED");
-
-// // app.get("/test", (req, res) => {
-// //     res.send("WORKING NOW");
-// // });
-
-// // const PORT = 8000;
-
-// // app.listen(PORT, () => {
-// //     console.log(`✅ Running on http://localhost:${PORT}`);
-// // });
-
-// import express from "express";
-// import protectedRoutes from "./routes/protectedRoutes.js";
-
-// const app = express();
-
-// app.use(express.json());
-
-// console.log("🔥 SERVER STARTED");
-
-// // main test
-// app.get("/test", (req, res) => {
-//     res.send("MAIN WORKING");
-// });
-
-// // routes
-// app.use("/api", protectedRoutes);
-
-// const PORT = 8000;
-
-// app.listen(PORT, () => {
-//     console.log(`✅ Running on http://localhost:${PORT}`);
-// });
-
 import express from "express";
 import protectedRoutes from "./routes/protectedRoutes.js";
 
@@ -63,18 +5,18 @@ const app = express();
 
 app.use(express.json());
 
-console.log("🔥 SERVER STARTED");
+console.log("SERVER STARTED");
 
-// Main test
+
 app.get("/test", (req, res) => {
     res.send("MAIN WORKING");
 });
 
-// Routes
+
 app.use("/api", protectedRoutes);
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log(`✅ Running on http://localhost:${PORT}`);
+    console.log(`Running on http://localhost:${PORT}`);
 });
